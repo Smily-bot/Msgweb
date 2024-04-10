58f9c2bcc95d0249e163d5f7c335087e7832ba57
smilybot <121439541+Smily-bot@users.noreply.github.com>
2024-04-10 18:24:36 +0330

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function sendMessage()
   { 
     const newMessageElement = document.createElement('div'); 
     let text2 = "you : ";
-    newMessageElement.innerText = text2 + message;
+    text2 = text2.concat(message);
+    newMessageElement.innerText = text2;
     messageContainer.appendChild(newMessageElement); 
     messageInput.value = ''; 
   } 
